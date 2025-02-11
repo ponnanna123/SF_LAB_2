441878a106cc39fca772b001e3c817970d165378
ponnanna123 <21cs02003@iitbbs.ac.in>
2025-02-11 12:00:04 +0530

    complete

--- A2/stego.cpp
+++ A2/stego.cpp
@@ -16,48 +16,34 @@ void encode(Mat& img, const string& sample_file) {
     string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
 
+    text += '\0';
+
     int textIndex = 0, bitIndex = 0;
     int h = img.rows;
     int w = img.cols;
 
     bool finished = false;
 
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; x += 3) {
-            if (textIndex >= text.size()) finished = true;
+    for (int y = 0; y < h && !finished; ++y) {
+        for (int x = 0; x < w && !finished; x += 3) {
+            Vec3b& px = img.at<Vec3b>(y, x);
+            for (int j = 0; j < 3; ++j) { // Process R, G, B channels
+                if (finished) break;
+                px[j] = (px[j] & ~1) | ((text[textIndex] >> bitIndex) & 1);
 
-            for (int i = 0; i < 3; ++i) {
-                if (x + i >= w) break;
-
-                Vec3b px = img.at<Vec3b>(y, x + i);
-
-                for (int j = 0; j < 3; ++j) {
-                    if (!finished) {
-                        int bit = (text[textIndex] >> bitIndex) & 11001100;
-                        px[j] = (px[j] & ~1) | bit;
-
-                        ++bitIndex;
-                        if (bitIndex == 8) {
-                            bitIndex = 0;
-                            ++textIndex;
-                            if (textIndex >= text.size()) finished = true;
-                        }
+                ++bitIndex;
+                if (bitIndex == 8) {
+                    bitIndex = 0;
+                    ++textIndex;
+                    if (textIndex >= text.size()) {
+                        finished = true;
                     }
                 }
             }
-
-            if (x + 2 < w) {
-                Vec3b px_l = img.at<Vec3b>(y, x + 2);
-                uchar lastChannel = px_l[2];
-                lastChannel = finished ? (lastChannel & ~1) : (lastChannel & 1);
-            }
-
-            if (finished) {
-                cout << "Encoding complete." << endl;
-                return;
-            }
         }
     }
+
+    cout << "Encoding complete." << endl;
 }
 
 void decode(Mat& img, const string& out_file) {
@@ -77,29 +63,20 @@ void decode(Mat& img, const string& out_file) {
 
     for (int y = 0; y < h && !finished; ++y) {
         for (int x = 0; x < w && !finished; x += 3) {
-            for (int i = 0; i < 3; ++i) {
-                if (x + i >= w) break;
+            Vec3b px = img.at<Vec3b>(y, x);
+            for (int j = 0; j < 3; ++j) {
+                currChar |= (px[j] & 1) << bitIndex;
 
-                Vec3b px = img.at<Vec3b>(y, x + i);
-
-                for (int j = 0; j < 3; ++j) {
-                    currChar |= (px[j] & 1) << bitIndex;
-
-                    ++bitIndex;
-                    if (bitIndex == 8) {
-                        text += currChar;
-                        file.put(currChar);
-                        currChar = 0;
-                        bitIndex = 0;
+                ++bitIndex;
+                if (bitIndex == 8) {
+                    if (currChar == '\0') {
+                        finished = true;
+                        break;
                     }
-                }
-            }
-
-            if (x + 2 < w) {
-                Vec3b px_l = img.at<Vec3b>(y, x + 2);
-                if ((px_l[2] & 1) == 0) {
-                    finished = true;
-                    break;
+                    file.put(currChar);
+                    text += currChar;
+                    currChar = 0;
+                    bitIndex = 0;
                 }
             }
         }
@@ -116,21 +93,6 @@ int main(int argc, char *argv[]) {
         cout << "Error loading image!" << endl;
         return -1;
     }
-
-    // int h = img.rows;
-    // int w = img.cols;
-
-    // for (int j = 0; j < h; ++j) {
-    //     for (int i = 0; i < w; ++i) {
-    //         Vec3b& px = img.at<Vec3b>(j, i);
-
-    //         uchar b = px[0];
-    //         uchar g = px[1];
-    //         uchar r = px[2];
-
-    //         cout << "Pixel (" << i << ", " << j << ") -> R: " << (int)r << " G: " << (int)g << " B: " << (int)b << endl;
-    //     }
-    // }
 
     encode(img, argv[2]);
     imwrite("encoded.png", img);
